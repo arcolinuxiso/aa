8e7dee0eb652389c95d4d8b67164909340e9012b
arcolinuxz <arcolinuxinfo@gmail.com>
2022-04-05 10:47:42 +0200

    archiso 62-1

--- build-archlinux-with-alis.sh
+++ build-archlinux-with-alis.sh
@@ -38,7 +38,7 @@ echo
 	isoLabel='archlinux-'$(date +%Y.%m.%d)'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 61-1"
+	archisoRequiredVersion="archiso 62-1"
 	buildFolder=$HOME"/AA-build"
 	outFolder=$HOME"/AA-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
